5b427f66cf203646cab2ccdb603588c85b4fac37
Cugatay <cagataykaydir@gmail.com>
2024-06-22 17:34:43 +0300

    with fonts

---
+++ .config/fish/tmux-sessionizer.fish
@@ -0,0 +1,15 @@
+#!/usr/bin/env fish
+
+# set window_path (find  ~/personal ~/work ~/.config -mindepth 1 -maxdepth 2 -type d | fzf)
+set window_path (find  ~/personal ~/work ~/.config -mindepth 1 -maxdepth 1 -type d | fzf)
+set window_name (basename "$window_path" | tr . _)
+
+if test -n "$window_path"
+  # tmux new-window -c "$window_path" # -n "$window_name" # "nvim"
+  cd "$window_path" # this is fish script
+  # pwd
+
+end
+
+
+# tmux send-keys -Rt 0 "cd /path/to/your/directory" C-m
